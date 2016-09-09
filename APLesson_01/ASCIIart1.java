b43acad36f98891e3535ce76d44718b48fd11ae7
kobinrong <robinkong99@gmail.com>
2016-09-09 11:02:49 -0700

    Stuff
    transferred problems from branch; fixed

---
+++ APLesson_01/ASCIIart1.java
@@ -0,0 +1,24 @@
+public class ASCIIart1
+{
+	public static void main(String[]args) 
+	{
+		
+		System.out.println("                 ______________________________");
+		System.out.println("                l                               l                             ");
+		System.out.println("              /                                   \\                          ");
+		System.out.println("             l                                       l                          ");
+		System.out.println("            /      o o o                o o o          \\                        ");
+		System.out.println("           l     o       o            o      o          l         ");
+		System.out.println("          /      o        o          o        o          \\");
+		System.out.println("          l      oooooooooo          oooooooooo           l");
+		System.out.println("          l      o        o          o        o           l");
+		System.out.println("          l       o     o             o      o            l");
+		System.out.println("          l          o                   o                l");
+		System.out.println("          l                                               l");
+		System.out.println("          l____________________________a space ship_______l");
+		System.out.println("         /                                                  \\");
+		System.out.println("        l                                                    l"); 
+		System.out.println("       l______________________________________________________l");
+		        
+	}
+}
---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,20 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+	{
+		int z = 67;
+		int t = 78;
+		int Product = z * t;
+		
+		System.out.println(z+" "+"multipled by"+" "+t+" "+"is"+" "+Product);
+		
+		String Name = "Kobin Rong";
+		String Address = "1010 A Street";
+		String City = "My City";
+		String Zip = "12345";
+		
+		System.out.println(Name + "\n" +  Address + "," + " " + My City + " " + Zip);
+		
+
+	}
+}
