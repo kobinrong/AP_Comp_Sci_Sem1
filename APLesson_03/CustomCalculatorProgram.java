098ac02c357beb8cb35dca57af9fb7b678e5c0d3
kobinrong <robinkong99@gmail.com>
2016-09-13 12:13:09 -0700

    Uploaded changes to custom calculator program

---
+++ APLesson_03/CustomCalculatorProgram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; //import Statement
+
+public class CustomCalculatorProgram
+{
+	public static void main(String[]args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompt user input 
+		System.out.println("I can multiply really large numbers... but not too large, haha! Give me a number.")
+		
+		int First num = keyboard.nextInt();
+		
+		System.out.println("Cool, give me the other number.")
+		
+		int Second num = keyboard.nextInt();
+		
+		
+	}
+}
