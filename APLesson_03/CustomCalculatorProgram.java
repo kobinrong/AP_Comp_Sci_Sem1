d7e35894ef40848425eb73b9abaa92e5e8c0bf91
kobinrong <robinkong99@gmail.com>
2016-09-15 11:35:21 -0700

    revised CustomCalculatorProgram

--- APLesson_03/CustomCalculatorProgram.java
+++ APLesson_03/CustomCalculatorProgram.java
@@ -7,13 +7,15 @@ public class CustomCalculatorProgram
 		Scanner keyboard = new Scanner(System.in);
 		
 		//prompt user input 
-		System.out.println("I can multiply really large numbers... but not too large, haha! Give me a number.")
+		System.out.println("I can multiply really large numbers... but not too large, haha! Give me a number.");
 		
-		int First num = keyboard.nextInt();
+		int First = keyboard.nextInt();
 		
-		System.out.println("Cool, give me the other number.")
+		System.out.println("Cool, give me the other number.");
 		
-		int Second num = keyboard.nextInt();
+		int Second = keyboard.nextInt();
+		int Result = First * Second;
+		System.out.println("The product of" + " " + First + " " + "and" + " " + Second + " " + "is" + " " + Result + ".");
 		
 		
 	}
