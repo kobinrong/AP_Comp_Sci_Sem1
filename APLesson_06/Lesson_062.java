6a2283d546ea5f30d0f68fb0374268e7708b3ab4
kobinrong <robinkong99@gmail.com>
2016-10-25 12:14:14 -0700

    lessons 6

---
+++ APLesson_06/Lesson_062.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Lesson_062
+{
+	public static void main(String[]args)
+	{
+		String word = "COMPSCI";
+		//System.out.println(word.length());
+		//System.out.println(word.indexOf("S"));
+		//System.out.println(word.indexOf("COMP"));
+		System.out.println(word.substring(1, 4));
+	}
+}
