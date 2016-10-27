063b7d6dcea58a0b492e2b9761c848cd3ec15c76
kobinrong <robinkong99@gmail.com>
2016-10-27 11:46:51 -0700

    uploaded lesson 6 labs

---
+++ APLesson_06/Lab_06GraphTable.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Lab_06GraphTable
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter an integer: ");
+		int num = kb.nextInt();
+		System.out.println("Please enter extent of table:  ");
+		int size = kb.nextInt();
+		
+	}
+}
--- APLesson_06/Lesson_062.java
+++ APLesson_06/Lesson_062.java
@@ -3,10 +3,21 @@ public class Lesson_062
 {
 	public static void main(String[]args)
 	{
-		String word = "COMPSCI";
+		//String word = "COMPSCI";
 		//System.out.println(word.length());
 		//System.out.println(word.indexOf("S"));
 		//System.out.println(word.indexOf("COMP"));
-		System.out.println(word.substring(1, 4));
+		//System.out.println(word.substring(1, 4)); //exclusive not inclusive 
+		//System.out.println(word.charAt(2));
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a word: ");
+		String word = kb.next();
+		
+		for(int i = 0; i < word.length(); i++)
+		{
+			//System.out.println(word.charAt(i));
+			System.out.println(word);
+			
+		}
 	}
 }
