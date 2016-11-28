d760a4e209bafe193853a518d197adeefb335e68
kobinrong <robinkong99@gmail.com>
2016-11-28 10:37:46 -0800

    uploaded lesson 8 + 9

--- APLesson_08/Lab08TreeDeg60.java
+++ APLesson_08/Lab08TreeDeg60.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
 public class Lab08TreeDeg60
 {
+	
 	public static void main(String[]args)
 	{
-		Scanner kb = new Scanner(System.in)
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please input a word: ");
+		String word = kb.next();
+		int stop = word.length();
+		tree(word, 0, stop);
+	}
+	public static void tree(String word,int start, int stop)
+	{
+		if (start <= stop)
+		{
+			System.out.printf("%15s\n", word.substring(0,start));
+			start++;
+			tree(word, start, stop);
+		}
 	}
 }
---
+++ APLesson_09/Lesson_09.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Lesson_09
+{
+	public static void main(String[]args)
+	{
+		String [] names = new String[5];
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter 5 names: ");
+		//fills the array; don't need parenthesis after length; length= a property, goes to a variable 
+		for(int i = 0; i < names.length; i++)
+		{
+			names[i] = kb.next();
+			
+		}
+		//print the array 
+		
+		for(String name : names)
+		{
+			System.out.print("\"" + name.length() + "\", ");
+		}//length is an operation in this case because deals with string 
+	}
+}
