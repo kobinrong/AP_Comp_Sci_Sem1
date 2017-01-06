5209187cf775d562d967e4afe9a97fea77771bf2
kobinrong <robinkong99@gmail.com>
2017-01-06 10:38:09 -0800

    lesson 11

---
+++ APLesson_11/APLesson_11.java
@@ -0,0 +1,27 @@
+public class APLesson_11
+{
+	public static void main(String[]args)
+	{
+		
+	//filling the array
+		int[][]values = new int [4][4];
+		int num = 1;
+		for(int i = 0; i < values.length; i++)
+		{
+			for(int j = 0; j < values[i].length; j++)
+			{
+				values[i][j] = num;
+				num++;
+			}
+		}
+		//print out the array
+		for(int i = 0; i< values.length; i++)
+		{
+			for(int j =0; j < values[i].length; j++)
+			{System.out.print(values[i][j] + "/t");
+		
+			}
+			System.out.println();
+		}
+	}
+}
